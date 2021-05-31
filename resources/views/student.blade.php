5abd0fd68a6e0e90e821709a1cc799ec5941f427
jmoletr <jmoler@uoc.edu>
2021-05-31 18:53:56 +0200

    Sidebar personalizado

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -1,41 +1,195 @@
-@extends('layouts.main')
-@section('contenido')
-<!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Panel Estudiante</h1>
-        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                class="fas fa-user fa-sm text-white-50"></i> Generate Report</a>
-    </div>
-    <div class="row">
-        @if($message = Session::get('Listo'))
-            <div class="col-12 alert alert-success alert-dismissible fade show" role="alert">
-                <h5>Mensaje: </h5>
-                <span>{{ $message }}</span>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Dashboard</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="{{asset ('/dash/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{asset ('/dash/css/sb-admin-2.min.css')}}" rel="stylesheet">
+    @yield('css')
+</head>
+
+<body id="page-top">
+
+<!-- Page Wrapper -->
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">--}}
+
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <i class="fas fa-laugh-wink"></i>
             </div>
-        @endif
-        <table class="table col-12 table-responsive">
-            <thead>
-                <tr>
-                    <td>Id</td>
-                    <td>Nombre</td>
-                    <td>Email</td>
-                    <td>username</td>
-                    <td>&nbsp;</td>
-                </tr>
-            </thead>
-            <tbody>
-            @foreach($students as $student)
-                <tr>
-                    <td>{{$student->id}}</td>
-                    <td>{{$student->name}}</td>
-                    <td>{{$student->email}}</td>
-                    <td>{{$student->username}}</td>
-                    <td>
-                        <button class="btn btn-round"> <i class="fa fa-trash"></i></button>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
+            <div class="sidebar-brand-text mx-3">BIENVENIDO</div>
+        </a>
+
+        <!-- Divider -->
+
+        <hr class="sidebar-divider my-0">
+        <li class="nav-item">
+            <a class="nav-link">
+                <span><strong>PANEL ESTUDIANTE</strong></span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/admin/">
+                <span>CURSOS ACTUALES:</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/admin/">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Admin</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/users/">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Usuarios</span>
+            </a>
+        </li>
+    </ul>
+
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+        <div id="content">
+
+            <!-- Topbar -->
+        @include('layouts.header')
+        <!-- End of Topbar -->
+
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
+                <!-- Page Heading -->
+                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 class="h3 mb-0 text-gray-800">Panel Estudiante</h1>
+                    <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                            class="fas fa-user fa-sm text-white-50"></i> Generate Report</a>
+                </div>
+                <div class="row">
+                    @if($message = Session::get('Listo'))
+                        <div class="col-12 alert alert-success alert-dismissible fade show" role="alert">
+                            <h5>Mensaje: </h5>
+                            <span>{{ $message }}</span>
+                        </div>
+                    @endif
+                    <table class="table col-12 table-responsive">
+                        <thead>
+                        <tr>
+                            <td>Id</td>
+                            <td>Nombre</td>
+                            <td>Email</td>
+                            <td>username</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($students as $student)
+                            <tr>
+                                <td>{{$student->id}}</td>
+                                <td>{{$student->name}}</td>
+                                <td>{{$student->email}}</td>
+                                <td>{{$student->username}}</td>
+                                <td>
+                                    <button class="btn btn-round"> <i class="fa fa-trash"></i></button>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Copyright &copy; Your Website 2021</span>
+                </div>
+            </div>
+        </footer>
+        <!-- End of Footer -->
+
     </div>
-@endsection
+    <!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">Dou you want close this session?</div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button class="btn btn-primary" type="submit">Logout</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Bootstrap core JavaScript-->
+<script src="{{asset ('/dash/vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{asset ('/dash/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="{{asset ('/dash/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{asset ('/dash/js/sb-admin-2.min.js')}}"></script>
+
+<!-- Page level plugins -->
+<script src="{{asset ('/dash/vendor/chart.js/Chart.min.js')}}"></script>
+
+<!-- Page level custom scripts -->
+<script src="{{asset ('/dash/js/demo/chart-area-demo.js')}}"></script>
+<script src="{{asset ('/dash/js/demo/chart-pie-demo.js')}}"></script>
+@yield('scripts')
+
+</body>
+
+</html>
+
+

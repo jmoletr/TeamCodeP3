959a8808d00e0c310fae0b46fb528c4a7fa702b5
Jorge Arrojo <arrojorge@uoc.edu>
2021-06-01 19:37:32 +0200

    listar asignaturas/clases mediante boton eye

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -24,12 +24,43 @@ class AdminController extends Controller
             ->orderby('id','DESC')
             ->get();
         $asignaturas = DB::table('users')
-            ->select('id','users.name','users.surname','courses.description')
+            ->select('id','users.name','users.surname','courses.description','courses.id_course')
             ->join('enrollment', 'users.id', '=', 'id_student')
             ->join('courses', 'enrollment.id_course', '=', 'courses.id_course')
             ->where('users.rol_id','3')
             //->select('users.*', 'contacts.phone', 'orders.price')
             ->get();
+        session(['asignaturas'=>$asignaturas]);
         return view('admin', ['asignaturas'=>$asignaturas]);
     }
+
+    public function store(Request $request){
+        // saber qué estudiante ha pulsado
+        $asignaturas = session('asignaturas');
+        //dd($asignaturas);
+        //dd($request->all());
+        $idStudent = $request->only('listarclass');
+        //dd($idStudent);
+        // hallar las class de ese estudiante
+        // $clases = DB::table('class')
+        //     ->select('id_class','class.name')
+        //     ->join('enrollment', 'enrollment.id_course', '=', 'courses.id_student')
+        //     ->where('enrollment.id_student',$idStudent)
+        //     ->get();
+        $clases = DB::table('class')
+            ->select('class.*','enrollment.id_student','users.name','users.surname','courses.description')
+            ->join('enrollment', 'enrollment.id_course', '=', 'class.id_course')
+            ->join('courses', 'enrollment.id_course', '=', 'courses.id_course')
+            ->join('users', 'users.id', '=', 'enrollment.id_student')
+            ->where('enrollment.id_student',$idStudent)
+            ->get();
+        // $clases = DB::table('class')
+        //     ->select('class.name')
+        //     ->join('users', 'users.id','=',$idStudent)
+        //     ->join('enrollment', 'enrollment.id_course', '=', 'class.id_course')
+        //     ->get();
+
+        return view('clases', ['clases'=>$clases]);
+    }
+       
 }
--- resources/views/clases.blade.php
+++ resources/views/clases.blade.php
@@ -2,7 +2,7 @@
 @section('contenido')
 <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Panel Estudiante</h1>
+        <h1 class="h3 mb-0 text-gray-800">Panel de Asignaturas</h1>
         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                 class="fas fa-user fa-sm text-white-50"></i> Generate Report</a>
     </div>
@@ -16,25 +16,24 @@
         <table class="table col-12 table-responsive">
             <thead>
                 <tr>
-                    <td>Estudiante</td>
+                    <td>Asignatura</td>
                     <td>Curso</td>
-                    <td>Ver detalles</td>
+                    <td>Alumno</td>
+                    <td>&nbsp;</td>
                 </tr>
             </thead>
             <tbody>
-            <!-- <?php // dd($asignaturas) ?> -->
-            @foreach($asignaturas as $asignatura)
-                <tr>
-                    <td>Alumno: {{$asignatura->name}} {{$asignatura->surname}}</td>
-                    <td>Curso: {{$asignatura->description}}</td>
-                    <td>
-                        <form action="/admin" method="POST">
-                        @csrf
-                        
-                            <button class="btn btn-round" type="submit" name="listarclass" value={{$asignatura->id}}> <i class="fa fa-eye"></i></button>
-                        </form>
-                    </td>
-                </tr>
+            <?php  //dd($clases) ?>
+            @foreach($clases as $clase)
+            <tr>
+                <td>{{$clase->name}}</td>
+                <td>{{$clase->name}}</td>
+                <td>{{$clase->name}} {{$clase->surname}}</td>
+                <td>
+                <!-- botonMostrar -->
+                &nbsp;
+                </td>
+            </tr>
             @endforeach
             </tbody>
         </table>

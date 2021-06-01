9716ce933c174e5cf775af4ebd5d7fa5937e19e4
Jorge Arrojo <arrojorge@uoc.edu>
2021-06-01 13:19:40 +0200

    Alumno y Cursos 1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
@@ -18,6 +19,17 @@ class AdminController extends Controller
 
 
     public function index(){
-        return view('admin');
+        $students = DB::table('students')
+            ->select('students.*')
+            ->orderby('id','DESC')
+            ->get();
+        $asignaturas = DB::table('users')
+            ->select('id','users.name','users.surname','courses.description')
+            ->join('enrollment', 'users.id', '=', 'id_student')
+            ->join('courses', 'enrollment.id_course', '=', 'courses.id_course')
+            ->where('users.rol_id','3')
+            //->select('users.*', 'contacts.phone', 'orders.price')
+            ->get();
+        return view('admin', ['asignaturas'=>$asignaturas]);
     }
 }

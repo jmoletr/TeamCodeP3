de18a99ce89da1105c7f2e3e6fbf20636b5780c9
Jorge Arrojo <arrojorge@uoc.edu>
2021-06-07 20:43:19 +0200

    fixed bugs

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -66,7 +66,6 @@ class StudentController extends Controller
                 ->join('courses', 'enrollment.id_course', '=', 'courses.id_course')
                 ->join('users', 'users.id', '=', 'enrollment.id_student')
                 ->where('enrollment.id_student',$idStudent)
-                ->where('class.id_class', $id_class)
                 ->get();
             $nota = DB::table('exams')
                 ->select('works.mark AS notaworks', 'exams.mark AS notaexamen','percentage.continuous_assessment AS ec', 'percentage.exams AS percentexamen', 'users.id AS id_student','class.id_class AS id_class','courses.id_course AS id_course')
